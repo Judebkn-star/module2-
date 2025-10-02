224df8f00295b3b2ca26e7ef093d4a74a4a329c5
Jude <jbuyikana@eugeniaschool.com>
2025-10-02 14:15:38 +0000

    2nd project

---
+++ Main2.py
@@ -0,0 +1,14 @@
+# TODO: Create a list of your favorite fruits
+
+favourite_fruits = ("Mango", "Strawberry", "Watermelon","Pineapple")
+print(favourite_fruits)
+
+# TODO: Add a new fruit to the end of the list
+
+# TODO: Remove the second fruit from the list
+
+# TODO: Sort the list alphabetically
+
+# TODO: Create a new list with the first three fruits
+
+# Print the original list and the new list
